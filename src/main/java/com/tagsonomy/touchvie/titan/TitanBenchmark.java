29e2914e29b414fff0854593a4cb436311bd417a
Guillermo Menéndez <guillermo.menendez@touchvie.com>
2016-04-06 12:56:41 +0200

    Added Titan benchmark and utilities file

---
+++ src/main/java/com/tagsonomy/touchvie/titan/TitanBenchmark.java
@@ -0,0 +1,50 @@
+package com.tagsonomy.touchvie.titan;
+
+import java.util.List;
+
+import com.thinkaurelius.titan.core.TitanGraph;
+
+public class TitanBenchmark {
+
+	public static void main(String[] args) {
+		
+		if (args.length != 2) {
+			System.out.println("Usage: java TitanBenchmark <graph_config_path> <card_id>");
+			System.exit(-1);
+		}
+		
+		try {
+			
+			TitanUtil util = new TitanUtil(args[0]);
+			TitanGraph graph = util.openGraph();
+			test(graph, args[1]);
+			
+		} catch (Exception ex) {
+			System.err.println(ex);
+			System.exit(-1);
+		}
+		
+		System.exit(0);
+	}
+
+	public static void test(final TitanGraph graph, final String cardId) throws Exception {
+
+		Long start = System.currentTimeMillis();
+		//List<String> cards = graph
+		List<Object> cards = graph
+				.traversal()
+				.V()
+				.has("card_id", cardId)
+				.out()
+				//.out()
+				.values("card_id")
+				.toList();
+		//		.toStream()
+		//		.map(o -> o.toString())
+		//		.collect(Collectors.toList());
+		System.out.println("Took " + (System.currentTimeMillis() - start) + "ms");
+		
+		for (Object card: cards)
+			System.out.println("  - " + (String)card);
+	}
+}
