c96737e385ce62e26fc37cda52bfe38dd5bc38d7
Guillermo Menéndez <guillermo.menendez@touchvie.com>
2016-04-06 13:47:52 +0200

    Benchmark allows querying multiple IDs

--- src/main/java/com/tagsonomy/touchvie/titan/TitanBenchmark.java
+++ src/main/java/com/tagsonomy/touchvie/titan/TitanBenchmark.java
@@ -7,44 +7,38 @@ import com.thinkaurelius.titan.core.TitanGraph;
 public class TitanBenchmark {
 
 	public static void main(String[] args) {
-		
-		if (args.length != 2) {
-			System.out.println("Usage: java TitanBenchmark <graph_config_path> <card_id>");
+
+		if (args.length < 2) {
+			System.out.println("Usage: java TitanBenchmark <graph_config_path> <card_id> ...");
 			System.exit(-1);
 		}
-		
+
 		try {
-			
+
 			TitanUtil util = new TitanUtil(args[0]);
 			TitanGraph graph = util.openGraph();
-			test(graph, args[1]);
-			
+			for (int i = 1; i < args.length; i++) 
+				test(graph, args[i]);
+
 		} catch (Exception ex) {
 			System.err.println(ex);
 			System.exit(-1);
 		}
-		
+
 		System.exit(0);
 	}
 
 	public static void test(final TitanGraph graph, final String cardId) throws Exception {
 
 		Long start = System.currentTimeMillis();
-		//List<String> cards = graph
-		List<Object> cards = graph
-				.traversal()
-				.V()
-				.has("card_id", cardId)
-				.out()
-				//.out()
-				.values("card_id")
-				.toList();
-		//		.toStream()
-		//		.map(o -> o.toString())
-		//		.collect(Collectors.toList());
+		// List<String> cards = graph
+		List<Object> cards = graph.traversal().V().has("card_id", cardId).out().out().values("card_id").toList();
+		// .toStream()
+		// .map(o -> o.toString())
+		// .collect(Collectors.toList());
 		System.out.println("Took " + (System.currentTimeMillis() - start) + "ms");
-		
-		for (Object card: cards)
-			System.out.println("  - " + (String)card);
+
+		for (Object card : cards)
+			System.out.println("  - " + (String) card);
 	}
 }
